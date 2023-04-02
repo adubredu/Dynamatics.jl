7b0c6c5483b6c9c5977168f47e65a1ee42d33302
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2023-04-02 10:50:54 -0400

    created partial function signatures

---
+++ src/dynamics.jl
@@ -0,0 +1,11 @@
+function generate_mass_matrix_function(filename::String)
+end
+
+function generate_momentum_matrix_function(root_name::String, filename::String)
+end
+
+function generate_inverse_dynamics_function(filename::String)
+end
+
+function generate_gravity_vector_function(filename::String)
+end
---
+++ src/kinematics.jl
@@ -0,0 +1,20 @@
+function generate_link_pose_function(link_name::String, filename::String)
+end
+
+function generate_relative_link_pose_function(from_link_name::String, to_link_name::String, filename::String)
+end
+
+function generate_geometric_jacobian_function(link_name::String, filename::String)
+end
+
+function generate_relative_geometric_jacobian_function(from_link_name::String, to_link_name::String, filename::String)
+end
+
+function generate_point_jacobian_function(link_name::String, filename::String)
+end
+
+function generate_relative_point_jacobian_function(from_link_name::String, to_link_name::String, filename::String)
+end
+
+function generate_center_of_mass_function(filename::String)
+end
