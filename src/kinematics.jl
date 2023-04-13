861e90a118b9cd6552fa909b56bd4870385fa585
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2023-04-13 00:49:36 -0400

    relative pose next

--- src/kinematics.jl
+++ src/kinematics.jl
@@ -21,17 +21,17 @@ end
 function generate_relative_link_pose_function(robot::Robot, from_link_name::String, to_link_name::String, filename::String)
 end
 
-function generate_geometric_jacobian_function(link_name::String, filename::String)
+function generate_geometric_jacobian_function(robot::Robot, link_name::String, filename::String)
 end
 
-function generate_relative_geometric_jacobian_function(from_link_name::String, to_link_name::String, filename::String)
+function generate_relative_geometric_jacobian_function(robot::Robot, from_link_name::String, to_link_name::String, filename::String)
 end
 
-function generate_point_jacobian_function(link_name::String, filename::String)
+function generate_point_jacobian_function(robot::Robot, link_name::String, filename::String)
 end
 
-function generate_relative_point_jacobian_function(from_link_name::String, to_link_name::String, filename::String)
+function generate_relative_point_jacobian_function(robot::Robot, from_link_name::String, to_link_name::String, filename::String)
 end
 
-function generate_center_of_mass_function(filename::String)
+function generate_center_of_mass_function(robot::Robot, filename::String)
 end
